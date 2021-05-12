379e2678955a3b4e358c092e93c66d84a9155944
mbcordeiro <matheusdebarroscordeiro@gmail.com>
2021-05-12 01:48:57 -0300

    SSG fecth request episodies

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,20 @@
-export default function Home() {
+export default function Home(props) {
   return (
-    <div>Hello</div>
+    <div>
+      <div>Index</div>
+      <p>{JSON.stringify(props.espisodies)}</p>
+    </div>
   )
+}
+
+export async function getStaticProps() {
+  const response = await fetch('http://localhost:3000/episodies')
+  const data = await response.json()
+
+  return {
+    props: {
+      episodies: data,
+    },
+    revalidate: 60 * 60 * 8,
+  }
 }
